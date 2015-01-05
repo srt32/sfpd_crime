ac7a2c16f36a57e59bdddb59f83bbba6ac609cbe
Simon Taranto <simon.taranto@gmail.com>
2015-01-04 17:10:18 -0800

    DOW plot and TOD plot

--- sfpd_crime_data.R
+++ sfpd_crime_data.R
@@ -1,6 +1,6 @@
 # pull in data
 file = file.path("/Users/Simon/Projects/Personal/sfpd_crime/SFPD_2014.csv")
-data = read.csv(file)
+data = read.csv(file, stringsAsFactors=FALSE)
 
 # filter by theft from auto
 thefts.auto = data[data$Descript == "GRAND THEFT FROM LOCKED AUTO",]
@@ -14,5 +14,17 @@ barplot(
   )
 )
 
+HoursStringToInt <- function(string) {
+  #as.numeric(strsplit(string, ":")[[1]][1])
+  (strsplit(string, ":")[[1]][1])
+}
+
 # histrogram by HOD
-str(thefts.auto)
+thefts.auto.tod <- thefts.auto[,c("IncidntNum", "Time")]
+thefts.auto.tod.normalized <- cbind(
+  thefts.auto.tod, TimeInt = mapply(HoursStringToInt, thefts.auto.tod$Time)
+)
+colnames(thefts.auto.tod.normalized) <- c("IncidntNum", "Time", "Hour")
+head(thefts.auto.tod.normalized)
+
+barplot(table(thefts.auto.tod.normalized$Hour))
